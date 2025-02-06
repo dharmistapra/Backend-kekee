1cf326b9698dcd81ceacccab3ad6c07e67282439
dharmistapra <dharmistapra.setblue@gmail.com>
2025-02-06 13:39:40 +0530

    server

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-import app from "../app.js"; // Import your existing Express app
+import app from "../app"; // Import your existing Express app
 
 export default (req, res) => {
   // Handle requests and pass them to the Express app

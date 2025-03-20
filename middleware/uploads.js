634b4e102fe2538349a0b2d29054af15a67f4ccc
dharmistapra <dharmistapra.setblue@gmail.com>
2025-03-20 13:49:39 +0530

    image

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -173,7 +173,7 @@ const uploadConfiguration = {
   importZip: multer({
     storage: dynamicStorage("importZip"),
     fileFilter: zipFile,
-    limits: { fileSize: 10000000 * 5 },
+    limits: { fileSize: 1000000000 * 5 },
   }).single("zip"),
 
   paymentMethod: multer({

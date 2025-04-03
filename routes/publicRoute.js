a49fc749d893b83126dece18646f86e41b620953
Shaikh Asjad <shaikhasjad29@gmail.com>
2025-04-03 10:08:08 +0530

    Some chnages

--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -6,7 +6,7 @@ import {
 import { getHomeBanner } from "../controller/admin/homeBanner.js";
 import { getSocialMedia } from "../controller/admin/socialMediaIcon.js";
 import { getAllCurrency } from "../controller/admin/currency.js";
-import { getAllCms, getCms } from "../controller/admin/cms.js";
+import { getAllCms, getBankdetails, getCms } from "../controller/admin/cms.js";
 import {
   getCategories,
   getCategoryCollection,
@@ -247,5 +247,6 @@ router.get("/categorycollection", getCategoryCollection);
 router.post("/contact-us", contactUsSchema, postContactUs);
 
 router.get("/websetting", getWebSetting);
+router.get("/bank/details", getBankdetails)
 
 export default router;

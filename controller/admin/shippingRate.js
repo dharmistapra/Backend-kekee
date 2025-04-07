f4d9ae280ecee44c89d270cff95305bd9aae694e
Shaikh Asjad <shaikhasjad29@gmail.com>
2025-04-07 18:28:27 +0530

    some chnages

--- controller/admin/shippingRate.js
+++ controller/admin/shippingRate.js
@@ -1,5 +1,5 @@
 import prisma from "../../db/config.js";
-import { deleteData, updateStatus } from "../../helper/common.js";
+import { deleteData } from "../../helper/common.js";
 
 const postShippingRate = async (req, res, next) => {
     try {
@@ -91,4 +91,87 @@ const deleteShippingRate = async (req, res, next) => {
     }
 };
 
-export { postShippingRate, puttShippingRate, deleteShippingRate }
+
+const getShippingMethod = async (req, res, next) => {
+    try {
+        const { country, weight, totalOrder } = req.query;
+        const normalizedCountry = country.charAt(0).toUpperCase() + country.slice(1).toLowerCase();
+        const shippingZones = await prisma.shippingZone.findMany({
+            where: {
+                countries: {
+                    has: normalizedCountry,
+                },
+            },
+            select: {
+                id: true,
+                name: true,
+                ShippingZoneAddRate: {
+                    where: {
+                        AND: [
+                            {
+                                OR: [
+                                    {
+                                        minWeight: { lte: parseFloat(weight) },
+                                        maxWeight: { gte: parseFloat(weight) }
+                                    },
+                                    {
+                                        minWeight: null,
+                                        maxWeight: null
+                                    }
+                                ]
+                            },
+                            {
+                                OR: [
+                                    {
+                                        minprice: { lte: parseFloat(totalOrder) },
+                                        maxprice: { gte: parseFloat(totalOrder) }
+                                    },
+                                    {
+                                        minprice: null,
+                                        maxprice: null
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    select: {
+                        id: true,
+                        name: true,
+                        price: true,
+                        description: true,
+                        type: true,
+                        selectedOption: true,
+                        minWeight: true,
+                        maxWeight: true,
+                        minprice: true,
+                        maxprice: true
+                    }
+                }
+            }
+        });
+
+        const availableRates = shippingZones.flatMap(zone =>
+            zone.ShippingZoneAddRate.map(rate => ({
+                zoneId: zone.id,
+                zoneName: zone.name,
+                ...rate
+            }))
+        );
+
+        if (availableRates.length === 0) {
+            return res.status(404).json({ message: "No shipping method found for the given criteria." });
+        }
+
+        return res.status(200).json({
+            isSuccess: true,
+            "message": "Shipping method get successfully",
+            shippingMethods: availableRates
+        });
+
+    } catch (err) {
+        const error = new Error("Something went wrong, please try again!");
+        next(error);
+    }
+};
+
+export { postShippingRate, puttShippingRate, deleteShippingRate, getShippingMethod }
